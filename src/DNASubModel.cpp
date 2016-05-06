8f80e98fae76d9c97e7a6fc9a767ccffdf3acba4
zhengqi <zhengqi@gricelabstation1.MED.UPENN.EDU>
2016-05-06 14:56:57 -0400

    introduce the DNASubModel base class

---
+++ src/DNASubModel.cpp
@@ -0,0 +1,110 @@
+/*
+ * DNASubModel.cpp
+ *
+ *  Created on: Apr 1, 2016
+ *      Author: zhengqi
+ */
+
+#include <vector>
+#include <set>
+#include <vector>
+#include "DNASubModel.h"
+
+namespace EGriceLab {
+using namespace std;
+using namespace Eigen;
+
+void DNASubModel::updateRate() {
+	Q = R * pi;
+	for(Matrix4d::Index i = 0; i < R.rows(); ++i) {
+		double q = 0;
+		for(Matrix4d::Index j = 0; j < R.cols(); ++j)
+			if(i != j)
+				q -= Q(i, j);
+		Q(i, i) = q;
+	}
+}
+
+void DNASubModel::scaleRate() {
+	double beta = pi.dot(Q.diagonal());
+	Q /= -beta;
+}
+
+/** Estimate the substitution parameters using Goldman algorithm */
+Matrix4d DNASubModel::estimateSubRateGoldman(const PhyloTree* tree) {
+	assert(tree->isRooted());
+	Matrix4d freq = Matrix4d::Zero();
+	const set<const PhyloTree::PhyloTreeNode*>& leafSet = tree->dfsLeaves(); // get all nodes
+	vector<const PhyloTree::PhyloTreeNode*> leaves(leafSet.begin(), leafSet.end()); // transfer into a list
+	/* pair-wise count of mutations */
+	for(vector<const PhyloTree::PhyloTreeNode*>::size_type i = 0; i < leaves.size() - 1; ++i)
+		for(vector<const PhyloTree::PhyloTreeNode*>::size_type j = i + 1; j < leaves.size(); ++j)
+			freq += updateParams2Seq(leaves[i], leaves[j]);
+	return freq;
+}
+
+/** Estimate the substitution parameters using Gojobori algorithm */
+Matrix4d DNASubModel::estimateSubRateGojobori(const PhyloTree* tree) {
+	assert(tree->isRooted());
+	Matrix4d freq = Matrix4d::Zero();
+	const set<const PhyloTree::PhyloTreeNode*>& leafSet = tree->dfsLeaves(); // get all leaves
+	for(set<const PhyloTree::PhyloTreeNode*>::const_iterator it = leafSet.begin(); it != leafSet.end(); ++it) {
+		for(const PhyloTree::PhyloTreeNode* node = *it; !node->isRoot(); node = node->parent) {
+			if(node == *it)
+				continue; /* ignore itself */
+			const set<const PhyloTree::PhyloTreeNode*>& siblingLeafSet = tree->dfsLeaves(tree->getSibling(node));
+			vector<const PhyloTree::PhyloTreeNode*> siblingLeaves(siblingLeafSet.begin(), siblingLeafSet.end());
+			assert(siblingLeaves.size() >= 2); /* make sure there exists triples */
+			for(vector<const PhyloTree::PhyloTreeNode*>::size_type i = 0; i < siblingLeaves.size() - 1; ++i)
+				for(vector<const PhyloTree::PhyloTreeNode*>::size_type j = i + 1; j < siblingLeaves.size(); ++j)
+					freq += updateParams3Seq(*it, siblingLeaves[i], siblingLeaves[j]);
+		}
+	}
+	return freq;
+}
+
+Matrix4d updateParams2Seq(const PhyloTree::PhyloTreeNode* seq1, const PhyloTree::PhyloTreeNode* seq2) {
+	assert(seq1->seq.getAbc() == seq2->seq.getAbc() || *(seq1->seq.getAbc()) == *(seq2->seq.getAbc()));
+	assert(seq1->seq.length() == seq2->seq.length());
+	Matrix4d freq = Matrix4d::Zero();
+
+	const DigitalSeq::size_type L = seq1->seq.length();
+	for(DigitalSeq::size_type i = 0; i < L; ++i)
+		if(seq1->seq[i] >= 0 && seq2->seq[i] >= 0) // both not a gap
+			freq(seq1->seq[i], seq2->seq[i])++;
+	return freq;
+}
+
+Matrix4d updateParams3Seq(const PhyloTree::PhyloTreeNode* outer,
+		const PhyloTree::PhyloTreeNode* seq1, const PhyloTree::PhyloTreeNode* seq2) {
+	assert(outer->seq.getAbc() == seq1->seq.getAbc() && outer->seq.getAbc() == seq2->seq.getAbc() ||
+			*(outer->seq.getAbc()) == *(seq1->seq.getAbc()) && *(outer->seq.getAbc()) == *(seq2->seq.getAbc()));
+	assert(outer->seq.length() == seq1->seq.length() && outer->seq.length() == seq2->seq.length());
+	Matrix4d freq = Matrix4d::Zero();
+
+	const DigitalSeq::size_type L = outer->seq.length();
+	for(DigitalSeq::size_type i = 0; i < L; ++i) {
+		int8_t b0 = outer->seq[i];
+		int8_t b1 = seq1->seq[i];
+		int8_t b2 = seq2->seq[i];
+		int8_t bc; // ancestor of b0, b1 and b2
+		if(!(b0 >= 0 && b1 >= 0 &&  b2 >= 0)) // ignore any gaps
+			continue;
+		if(b0 == b1 && b0 == b2) // no change
+			bc = b0;
+		else if(b0 == b1 && b0 != b2) // change is between bc->b2
+			bc = b0;
+		else if(b0 == b2 && b0 != b1) // change is between bc->b1
+			bc = b0;
+		else if(b0 != b1 && b0 != b2 && b1 == b2) // change is between bc->b0
+			bc = b1;
+		else // all different, cannot guess
+			continue;
+		freq(bc, b0)++;
+		freq(bc, b1)++;
+		freq(bc, b2)++;
+	}
+	return freq;
+}
+
+} /* namespace EGriceLab */
---
+++ src/DNASubModel.h
@@ -0,0 +1,105 @@
+/*
+ * DNASubModel.h
+ *
+ *  Created on: Apr 1, 2016
+ *      Author: zhengqi
+ */
+
+#ifndef DNASUBMODEL_H_
+#define DNASUBMODEL_H_
+#include <string>
+#include <eigen3/Eigen/Dense>
+#include "DegenAlphabet.h"
+#include "MSA.h"
+#include "StringUtils.h"
+#include "PhyloTree.h"
+
+namespace EGriceLab {
+
+using std::string;
+using Eigen::Vector4d;
+using Eigen::VectorXd;
+using Eigen::Matrix4d;
+
+class DNASubModel {
+public:
+	/* nested class and enums */
+	enum Base { A, C, G, T };
+
+public:
+	/* Constructors */
+	/* virtual destructor, do nothing */
+	virtual ~DNASubModel() { }
+
+	/* member methods */
+	/** get model name */
+	string getName() const;
+
+	/** pure virtual method to prepare rate parameters R */
+	virtual void updateParam(const Matrix4d& freq) = 0;
+
+	/* init (normalize) base frequency vector pi */
+	void initFreq();
+
+	/** update Q from R and pi */
+	void updateRate();
+
+	/** Scale the Q so that a branch lenth of 1 yields one expected change */
+	void scaleRate();
+
+	/** Estimate the substitution frequency from an multiple sequence alignment using a given algorithm */
+	Matrix4d estimateSubRate(const PhyloTree* tree, const string& method = "Gojobori");
+
+protected:
+	/** Estimate the substitution parameters using Goldman algorithm */
+	Matrix4d estimateSubRateGoldman(const PhyloTree* tree);
+
+	/** Estimate the substitution parameters using Goldman algorithm */
+	Matrix4d estimateSubRateGojobori(const PhyloTree* tree);
+
+	/** Update the parameters using two-sequence method */
+	virtual Matrix4d updateParams2Seq(const PhyloTree::PhyloTreeNode* seq1, const PhyloTree::PhyloTreeNode* seq2);
+	/** Update the parameters using three-sequence method */
+	virtual Matrix4d updateParams3Seq(const PhyloTree::PhyloTreeNode* outer,
+			const PhyloTree::PhyloTreeNode* seq1, const PhyloTree::PhyloTreeNode* seq2);
+
+public:
+	/** pure virtual method to get the Prob matrix given branch length and optionally rate factor
+	 * @param v  branch length in the unit time
+	 * @param h  rate factor in variable rate ML estimates
+	 * @return  Probability rate matrix between for Bases
+	 */
+	virtual Matrix4d Pr(double t, double r) const = 0;
+
+
+private:
+	string name;
+
+	Vector4d pi; /* base frequency of ATCG */
+	Matrix4d R; /* Symmetric rate parameter matrix */
+	Matrix4d Q; /* rate matrix, Q = pi X R for i!=j */
+};
+
+inline string DNASubModel::getName() const {
+	return name;
+}
+
+inline void DNASubModel::initFreq() {
+	pi /= pi.sum();
+}
+
+inline Matrix4d DNASubModel::estimateSubRate(const PhyloTree *tree, const string& method) {
+	if(toLower(method) == "goldman")
+		return estimateSubRateGoldman(tree);
+	else if(toLower(method) == "gojobori")
+		return estimateSubRateGojobori(tree);
+	else {
+		std::cerr << "Unknown parameter estimation method " << method << std::endl;
+		abort();
+		return Matrix4d::Zero();
+	}
+}
+
+} /* namespace EGriceLab */
+
+#endif /* SRC_DNASUBMODEL_H_ */
