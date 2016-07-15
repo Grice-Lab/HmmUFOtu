f162ee9e6c4f1ccd7ac4515bda0597960eac5b16
zhengqi <zhengqi@gricelabstation1.MED.UPENN.EDU>
2016-07-15 10:41:06 -0400

    New tests introduced

--- test/dm_mixture_train1.cpp
+++ test/dm_mixture_train1.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <eigen3/Eigen/Dense>
+#include <cstdlib>
 #include "MSA.h"
-#include "DirichletDensity.h"
+#include "DirichletMixture.h"
 
 using namespace std;
 using namespace Eigen;
@@ -10,13 +11,14 @@ using namespace EGriceLab;
 using namespace EGriceLab::Math;
 
 int main(int argc, char *argv[]) {
-	if(argc != 3) {
-		cerr << "Usage:  " << argv[0] << " MSA-DB DM-OUTFILE" << endl;
+	if(argc != 4) {
+		cerr << "Usage:  " << argv[0] << " MSA-DB NUM-MIXTURE DM-OUTFILE" << endl;
 		return -1;
 	}
 
 	ifstream in(argv[1]);
-	ofstream out(argv[2]);
+	int L = ::atoi(argv[2]);
+	ofstream out(argv[3]);
 	if(!in.is_open()) {
 		cerr << "Unable to open " << argv[1] << endl;
 		return -1;
@@ -39,19 +41,19 @@ int main(int argc, char *argv[]) {
 	MatrixXd data(msa->getAbc()->getSize(), msa->getCSLen());
 	unsigned M = 0;
 	for(unsigned j = 0; j < data.cols(); ++j) {
-		if(msa->symWFrac(j) >= 0.5) // an insertion, non-conserved region
+		if(msa->symWFrac(j) >= 0.5) // a match position conserved region
 			data.col(M++) = msa->symWFreq(j);
 	}
 	data.conservativeResize(data.rows(), M);
-//	cerr << "Residual frequency data read data:" << endl << data.rowwise().sum() << endl;
+	cerr << "Residual frequency data read, data:" << endl << data.rowwise().sum() << endl;
 
-	DirichletDensity dm(data.rows());
-	cerr << "Dirichlet Density model constructed" << endl;
+	DirichletMixture dm(data.rows(), L);
+	cerr << "Dirichlet Mixture model constructed" << endl;
 	double c = dm.trainML(data);
-	cerr << "Dirichlet Density model Maximum Likelihood trained" << endl;
+	cerr << "Dirichlet Mixture model EM trained" << endl;
 	out << "Final cost: " << c << endl;
 	out << dm << endl;
-	cerr << "Dirichlet Density model written" << endl;
+	cerr << "Dirichlet Mixture model written" << endl;
 
 	return 0;
 }
--- test/dm_mixture_train2.cpp
+++ test/dm_mixture_train2.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <eigen3/Eigen/Dense>
 #include "MSA.h"
-#include "DirichletDensity.h"
+#include "DirichletMixture.h"
 
 using namespace std;
 using namespace Eigen;
@@ -10,13 +10,14 @@ using namespace EGriceLab;
 using namespace EGriceLab::Math;
 
 int main(int argc, char *argv[]) {
-	if(argc != 3) {
-		cerr << "Usage:  " << argv[0] << " MSA-DB DM-OUTFILE" << endl;
+	if(argc != 4) {
+		cerr << "Usage:  " << argv[0] << " MSA-DB NUM-MIX DM-OUTFILE" << endl;
 		return -1;
 	}
 
 	ifstream in(argv[1]);
-	ofstream out(argv[2]);
+	int L = atoi(argv[2]);
+	ofstream out(argv[3]);
 	if(!in.is_open()) {
 		cerr << "Unable to open " << argv[1] << endl;
 		return -1;
@@ -39,19 +40,31 @@ int main(int argc, char *argv[]) {
 	MatrixXd data(msa->getAbc()->getSize(), msa->getCSLen());
 	unsigned M = 0;
 	for(unsigned j = 0; j < data.cols(); ++j) {
-		if(msa->symWFrac(j) >= 0.5) // an insertion, non-conserved region
+		if(msa->symWFrac(j) >= 0.5) // a match position conserved region
 			data.col(M++) = msa->symWFreq(j);
 	}
 	data.conservativeResize(data.rows(), M);
-//	cerr << "Residual frequency data read data:" << endl << data.rowwise().sum() << endl;
+	VectorXd rowMin = data.rowwise().minCoeff();
+	cerr << "rowMin:" << rowMin << endl;
+	VectorXd leftFac(data.rows());
+	VectorXd rightFac(data.rows());
+	leftFac << 1, 10, 20, 40;
+	rightFac << 40, 20, 10, 1;
+	for(int t = 0; t < M; ++t)
+		if(t < M / 2)
+			data.col(t).array() *= leftFac.array();
+		else
+			data.col(t).array() *= rightFac.array();
 
-	DirichletDensity dm(data.rows());
-	cerr << "Dirichlet Density model constructed" << endl;
+	cerr << "Residual frequency data read" << endl;
+
+	DirichletMixture dm(data.rows(), L);
+	cerr << "Dirichlet Mixture model constructed" << endl;
 	double c = dm.trainML(data);
-	cerr << "Dirichlet Density model Maximum Likelihood trained" << endl;
+	cerr << "Dirichlet Mixture model EM trained" << endl;
 	out << "Final cost: " << c << endl;
 	out << dm << endl;
-	cerr << "Dirichlet Density model written" << endl;
+	cerr << "Dirichlet Mixture model written" << endl;
 
 	return 0;
 }
