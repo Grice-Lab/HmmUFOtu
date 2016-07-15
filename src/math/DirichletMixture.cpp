93618c89376944889f62377ef84003916d11ab8e
zhengqi <zhengqi@gricelabstation1.MED.UPENN.EDU>
2016-07-15 10:40:23 -0400

    Introduction of the new DirichletMixture class

---
+++ src/math/DirichletMixture.cpp
@@ -0,0 +1,246 @@
+/*
+ * DirichletMixture.cpp
+ *
+ *  Created on: Jul 6, 2016
+ *      Author: zhengqi
+ */
+
+#include <cassert>
+#include <cmath>
+#include <boost/math/special_functions/digamma.hpp>
+#include <iostream>
+#include <algorithm>
+#include "DirichletMixture.h"
+
+namespace EGriceLab {
+namespace Math {
+
+using namespace std;
+using namespace Eigen;
+using boost::math::digamma;
+
+/* private comparator functions */
+struct MyFreqComparator {
+	MyFreqComparator(const MatrixXd& data) : data(data) { }
+
+	bool operator() (int i, int j) {
+		VectorXd f1 = data.col(i);
+		VectorXd f2 = data.col(j);
+		for(MatrixXd::Index k = 0; k < data.rows(); ++k) {
+			if(f1(k) != f2(k))
+				return f1(k) < f2(k); // higher priority satisfied
+		}
+		return false; // all frequencies are equal
+	}
+	const MatrixXd& data;
+
+};
+
+VectorXd DirichletMixture::meanPostP(const VectorXd& data) const {
+	assert(data.size() == alpha.rows());
+	int K = alpha.rows();
+	/* calculate the beta function part */
+	VectorXd logB(L);
+	for(int j = 0; j < L; ++j)
+		logB(j) = lbeta(alpha.col(j) + data) - lbeta(alpha.col(j));
+	logB.array() -= logB.maxCoeff(); // normalize to avoid numeric overflow
+
+	VectorXd X = VectorXd::Zero(K);
+	double dataSum = data.sum();
+	RowVectorXd alphaSum = alpha.colwise().sum();
+	for(int i = 0; i < K; ++i)
+		for(int j = 0; j < L; ++j)
+			X(i) += q(j) * ::exp(logB(j)) * (alpha(i, j) + data(i)) / (alphaSum(j) + dataSum);
+	return X / X.sum();
+}
+
+MatrixXd DirichletMixture::weightGradient(const MatrixXd& data) const {
+	assert(data.rows() == alpha.rows());
+	int K = alpha.rows();
+	MatrixXd::Index M = data.cols();
+
+	MatrixXd grad(K, L);
+	RowVectorXd alphaSum = alpha.colwise().sum();
+	RowVectorXd nSum = data.colwise().sum();
+
+	/* calculate the compPostP on each training data column */
+	MatrixXd compP(L, M);
+	for(MatrixXd::Index t = 0; t < M; ++t)
+		compP.col(t) = compPostP(data.col(t));
+	VectorXd compS = compP.rowwise().sum();
+
+	for(int j = 0; j < L; ++j) { // for each component
+		for(int i = 0; i < K; ++i) { // for each category
+			double S = 0;
+			for(MatrixXd::Index t = 0; t < M; ++t) {
+				S += compP(j, t) * (digamma(static_cast<double> (data(i, t)) + static_cast<double> (alpha(i, j)))
+						- digamma(static_cast<double> (nSum(t)) + static_cast<double> (alphaSum(j)) ));
+			}
+			grad(i, j) = alpha(i, j) * (compS(j) * (digamma(static_cast<double> (alphaSum(j)))
+					- digamma(static_cast<double> (alpha(i, j))) ) + S);
+		}
+	}
+	return grad;
+}
+
+double DirichletMixture::trainML(const MatrixXd& data, double eta, int maxIt,
+		double epsilonCost, double epsilonParams) {
+	assert(data.rows() == alpha.rows());
+	/* initiate the parameters using moment-matctching */
+	momentInit(data);
+
+	MatrixXd::Index M = data.cols();
+	/* EM algorithm to update both the Dirichlet parameters and mixture coefficients */
+	double c = cost(data);
+	for(int it = 0; maxIt <= 0 || it < maxIt; ++it) { // infinite loop to be terminated within
+		/* M step, maximize the parameters using gradient descent */
+		MatrixXd wGrad = weightGradient(data);
+		/* update weight and parameters */
+		w += eta * wGrad;
+		MatrixXd alphaOld(alpha);
+		alpha = w.array().exp();
+
+		/* check the new parameters for over-fitting */
+		if((alpha.array() == 0).any()) {
+			cerr << "Potential over-fitting detected. Please choose another MSA training set" << endl;
+			return NAN;
+		}
+		if(q.isZero()) {
+			cerr << "Potential unused (zero-coefficient) mixture component detected. Please use a smaller q for training" << endl;
+			return NAN;
+		}
+		/* calculate new cost */
+		double cNew = cost(data);
+		double deltaC = c - cNew;
+//		fprintf(stderr, "c:%lg cNew:%lg deltaC:%lg\n", c, cNew, deltaC);
+
+		/* E step, update the mixture coefficients using iteration */
+		VectorXd qNew = VectorXd::Zero(L);
+		for(MatrixXd::Index t = 0; t < M; ++t)
+			qNew += compPostP(data.col(t));
+		qNew /= static_cast<double> (M);
+		c = cNew;
+
+		/* termination check */
+		if(alpha.isApprox(alphaOld, epsilonParams) && qNew.isApprox(q, epsilonParams) && deltaC >= 0 && deltaC < epsilonCost)
+			break;
+		q = qNew;
+	}
+	return c;
+}
+
+double DirichletMixture::pdf(const VectorXd& data) const {
+	assert(data.size() == alpha.rows());
+	int K = alpha.rows();
+	double dataNorm = data.sum();
+
+	double p = 0;
+	for(int j = 0; j < L; ++j) {
+		/* constant part */
+		double alphaNorm = alpha.col(j).sum();
+		double logC = lgamma(dataNorm + 1) + lgamma(alphaNorm) - lgamma(dataNorm + alphaNorm);
+		/* product part */
+		double logS = 0;
+		for(int i = 0; i < K; ++i) {
+			logS += lgamma(static_cast<double> (data(i)) + static_cast<double> (alpha(i, j)))
+						- lgamma(static_cast<double> (data(i)) + 1)
+						- lgamma(static_cast<double> (alpha(i, j)));
+		}
+		p += q(j) * ::exp(logC + logS);
+	}
+	return p;
+}
+
+double DirichletMixture::lbeta(const VectorXd& x) {
+	double s = 0;
+	for(VectorXd::Index i = 0; i != x.size(); ++i)
+		s += lgamma(static_cast<double> (x(i)));
+	return s - lgamma(x.sum());
+}
+
+VectorXd DirichletMixture::compPostP(const VectorXd& data) const {
+	assert(data.size() == alpha.rows());
+	int K = alpha.rows();
+	VectorXd logP(L); // un-normalized component posterior probability
+	double dataSum = data.sum();
+	RowVectorXd alphaSum = alpha.colwise().sum();
+	for(int j = 0; j < L; ++j) {
+		double C = lgamma(dataSum + 1) + lgamma(static_cast<double> (alphaSum(j)))
+				- lgamma(dataSum + static_cast<double> (alphaSum(j))); // const part
+		double S = 0; // product part
+		for(int i = 0; i < K; ++i)
+			S += lgamma(static_cast<double> (data(i)) + static_cast<double> (alpha(i, j)))
+				- lgamma(static_cast<double> (data(i)) + 1)
+				- lgamma(static_cast<double> (alpha(i, j)));
+		logP(j) = C + S;
+	}
+	VectorXd p = q.array() * logP.array().exp();
+	return p / p.sum();
+}
+
+ostream& DirichletMixture::print(ostream& out) const {
+	out << "Dirichlet Mixture Model" << endl;
+	out << "K: " << getK() << " L:" << L << endl;
+	out << "Mixture coefficient q:" << endl << q << endl;
+	for(int j = 0; j < L; ++j)
+		out << "Component " << j << " alpha:" << alpha.col(j).transpose() << endl;
+	return out;
+}
+
+void DirichletMixture::momentInit(MatrixXd data) {
+	int K = getK();
+	int M = data.cols();
+	if(M < 2 * L)
+		return; /* at least 2 data required for each component */
+
+	/* Normalize the column sum, so the observed data follows Dirichlet-Multinomial distribution */
+	double N = data.colwise().sum().maxCoeff();
+	for(int t = 0; t < M; ++t)
+		data.col(t) *= N / data.col(t).sum();
+
+	/* Sort data according to relative frequency of each base */
+	int* idx = new int[M];
+	for(int t = 0; t < M; ++t)
+		idx[t] = t;
+	std::sort(idx, idx + M, MyFreqComparator(data));
+	/* Sort the data */
+	MatrixXd dataSorted(K, M);
+	for(int t = 0; t < M; ++t)
+		dataSorted.col(t) = data.col(idx[t]);
+//		dataSorted.col(t) = data.col(t);
+
+	delete[] idx;
+
+	/* Devide the data to L equal size categories and do moment-matching */
+	for(int j = 0; j < L; ++j) {
+		int blockStart = j * M / L;
+		MatrixXd block = dataSorted.block(0, blockStart, K, M / L);
+		//	cerr << "Calculating moments" << endl;
+		/* calculate the Mean (1st-moment) and Var (2nd-moment) of the observed frequencies */
+		VectorXd blockMean = block.rowwise().mean();
+		VectorXd blockVar = (block.colwise() - blockMean).rowwise().squaredNorm() / (block.cols() - 1);
+
+		/* calculate parameter concentration try tring E(i) and Var(i) */
+		double alphaNorm = 0;
+		for(int i = 0; i < K; ++i) {
+			alphaNorm = (blockVar(i) - N * blockMean(i) + 1) / (blockMean(i) - 1 / N - blockVar(i));
+			if(alphaNorm > 0)
+				break;
+		}
+		if(alphaNorm <= 0) // do not use moment initiate for this component
+			continue;
+
+		alpha.col(j) = blockMean * alphaNorm / N;
+	}
+	w = alpha.array().log();
+}
+
+istream& DirichletMixture::read(istream& in) {
+	return in;
+}
+
+
+} /* namespace Math */
+} /* namespace EGriceLab */
+
+
---
+++ src/math/DirichletMixture.h
@@ -0,0 +1,111 @@
+/*
+ * DirichletMixture.h
+ *  Dirichlet Mixture model
+ *  Created on: Jul 6, 2016
+ *      Author: zhengqi
+ */
+
+#ifndef SRC_MATH_DIRICHLETMIXTURE_H_
+#define SRC_MATH_DIRICHLETMIXTURE_H_
+
+#include "DirichletModel.h"
+
+namespace EGriceLab {
+namespace Math {
+
+class DirichletMixture: public DirichletModel {
+public:
+	/* constructors */
+	/* default constructor disabled */
+/*	DirichletDensity();*/
+
+	/* construct a Dirichlet density with given categories and optionally estimated alpha */
+	DirichletMixture(int K, int L):
+		DirichletModel(K), L(L), q(L), w(K, L), alpha(K, L) /* initiate w and alpha to correct size */ {
+		assert(K >= MIN_K);
+		assert(L >= MIN_COMPONENT);
+		alpha.setConstant(DEFAULT_ALPHA);
+		w.setConstant(DEFAULT_WEIGHT);
+		q.setConstant(1.0 / L);
+	}
+
+	/* destructor, do nothing */
+	virtual ~DirichletMixture() { }
+
+	/* member methods */
+	/**
+	 * Calculate the mean posterior probability given this model an observed frequency
+	 * implement the base case abstract method
+	 */
+	virtual VectorXd meanPostP(const VectorXd& freq) const;
+
+	/**
+	 * Calculate the posterior probability of each component given the model parameters and an observed frequency
+	 */
+	VectorXd compPostP(const VectorXd& freq) const;
+
+	/**
+	 * Calculate the negative gradient of the weights (exp(parameters))
+	 * using current parameters and observed data
+	 * @return  the weight gradient matrix (K x L)
+	 */
+	MatrixXd weightGradient(const MatrixXd& data) const;
+
+	/**
+	 * Initiate the Dirichlet parameters using momenth-matching method
+	 * Implement the base class method
+	 */
+	virtual void momentInit(MatrixXd data);
+
+	/**
+	 * Do a maximum likelihood training of all parameters given a training data,
+	 * with M columns each an observed frequency vector of length and K (K * M matrix)
+	 * @return  cost at trained parameters, or NAN if over-fitting or numeric problem occured
+	 */
+	virtual double trainML(const MatrixXd& data,
+			double eta = DEFAULT_ETA, int maxIt = MAX_ITERATION,
+			double epsilonCost = DEFAULT_EPSILON_COST,
+			double epsilonParams = DEFAULT_EPSILON_PARAMS);
+
+	/**
+	 * Calculate the PDF of observing a data using this DM model
+	 * the lpdf must base on this method because the existence of the mixture coefficients
+	 */
+	double pdf(const VectorXd& freq) const;
+
+	/**
+	 * Calculate the log PDF of observing a data using this model
+	 * implemet the base abstract method
+	 */
+	virtual double lpdf(const VectorXd& freq) const {
+		return ::log(pdf(freq));
+	}
+
+	/* static methods */
+	/**
+	 * log Beta function defined on a vector value
+	 * Beta(X) = Pi(Gamma(xi)) / Gamma(|xi|)
+	 */
+	static double lbeta(const VectorXd& x);
+
+	/* implement base class private method */
+	virtual ostream& print(ostream& out) const;
+	virtual istream& read(istream& in);
+
+	/* member fields */
+private:
+	int L; // number of Dirichlet Mixture components
+	MatrixXd alpha; // Dirichlet Mixture parameters (K x L), with each column a Dirichlet parameter vector of length K
+	MatrixXd w; // Dirichlet Mixture weights (K x L), alpha = exp(w)
+	VectorXd q; // mixture coefficient with length L
+
+public:
+	static const int MIN_COMPONENT = 2; /* minimum number of components */
+	static const double DEFAULT_ALPHA = 1;
+	static const double DEFAULT_WEIGHT = 0;
+};
+
+} /* namespace Math */
+} /* namespace EGriceLab */
+
+#endif /* SRC_MATH_DIRICHLETMIXTURE_H_ */
