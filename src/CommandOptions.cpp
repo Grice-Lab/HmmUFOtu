ab5ce73bcca4d8c92d8759b7a1cec185125ba0c6
zhengqi <zhengqi@gricelabstation1.MED.UPENN.EDU>
2016-07-15 14:33:11 -0400

    New help class introduced

---
+++ src/CommandOptions.cpp
@@ -0,0 +1,27 @@
+/*
+ * CommandOptions.cpp
+ *
+ *  Created on: Jul 15, 2016
+ *      Author: zhengqi
+ */
+
+#include "CommandOptions.h"
+
+namespace EGriceLab {
+
+CommandOptions::CommandOptions(int argc, const char** argv) {
+	/* parse options */
+	for(int i = 1; i < argc; ++i) {
+		if(*argv[i] == '-') { /* a tag name */
+			if(i < argc - 1 && *argv[i+1] != '-') /* a tag value */
+				opts[argv[i]] = argv[++i];
+			else /* a flag tag */
+				opts[argv[i]] = ""; /* use empty value */
+		}
+		else /* a main opt */
+			mainOpts.push_back(argv[i]);
+	}
+}
+
+} /* namespace EGriceLab */
+
---
+++ src/CommandOptions.h
@@ -0,0 +1,49 @@
+/*
+ * CommandOptions.h
+ *
+ *  Created on: Jul 15, 2016
+ *      Author: zhengqi
+ */
+
+#ifndef SRC_COMMANDOPTIONS_H_
+#define SRC_COMMANDOPTIONS_H_
+
+#include <string>
+#include <set>
+#include <map>
+#include <vector>
+
+namespace EGriceLab {
+
+using std::string;
+using std::map;
+using std::vector;
+
+class CommandOptions {
+public:
+	CommandOptions(int argc, const char** argv);
+
+	bool hasOpt(const string& name) const {
+		return opts.find(name) != opts.end();
+	}
+
+	string getOpt(const string& name) const {
+		return hasOpt(name) ? opts.find(name)->second : "";
+	}
+
+	int numMainOpts() const {
+		return mainOpts.size();
+	}
+
+	string getMainOpt(int i) const {
+		return mainOpts.at(i);
+	}
+
+private:
+	vector<string> mainOpts; /* mandatory options not following any -tag-name */
+	map<string, string> opts; /* optional named options in -tag-name [value] pairs */
+};
+
+} /* namespace EGriceLab */
+
+#endif /* SRC_COMMANDOPTIONS_H_ */
