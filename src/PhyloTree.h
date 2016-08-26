18459f86845862ea074e73b4b5d3be1e9c6daed4
zhengqi <zhengqi@gricelabstation1.MED.UPENN.EDU>
2016-08-26 11:59:31 -0400

    First boost::spirit::qi based implementation

--- src/PhyloTree.h
+++ src/PhyloTree.h
@@ -10,158 +10,96 @@
 #define SRC_PHYLOTREE_H_
 
 #include <string>
-#include <set>
 #include <vector>
-#include <map>
+#include <limits>
+#include <iostream>
 #include <stdexcept>
 #include <cstddef>
+#include <cassert>
 #include <eigen3/Eigen/Dense>
 #include <boost/spirit/include/qi.hpp>
 #include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/variant/recursive_variant.hpp>
 #include <boost/spirit/include/phoenix_core.hpp>
 #include <boost/spirit/include/phoenix_operator.hpp>
 #include <boost/spirit/include/phoenix_fusion.hpp>
 #include <boost/spirit/include/phoenix_stl.hpp>
 
+#include "HmmUFOtuConst.h"
 #include "StringUtils.h"
 #include "SeqCommons.h"
+#include "DigitalSeq.h"
 #include "MSA.h"
 
 namespace EGriceLab {
 
 using std::string;
-using std::set;
+using std::vector;
+using std::istream;
+using std::ostream;
 using Eigen::Matrix4Xd;
 
-class PhyloTree {
-	/* nested types and enums */
-private:
-	enum NodeType { P /* parent */, L /* left */, R /* right */ };
-	class PhyloTreeNode;
+struct PhyloTree;
 
-	typedef PhyloTreeNode PTNode;
+typedef PhyloTree PT;
 
-	class PhyloTreeNode {
-	public:
-		/* constructors */
-		/* Default constructor */
-		PhyloTreeNode() : length(0) { }
+//typedef boost::variant<boost::recursive_wrapper<PT> > pt_node;
 
-		/* construct a node with a given DigitalSeq */
-		explicit PhyloTreeNode(const DigitalSeq& seq) : seq(seq), length(0) { }
-
-		/* construct a node with a given PrimarySeq */
-		explicit PhyloTreeNode(const PrimarySeq& seq) : seq(seq), length(0) { }
-
-		virtual ~PhyloTreeNode() { }
-
-		/* Member methods */
-		/** test parent of this node */
-		bool hasParent() const {
-			return neighbors.find(P) != neighbors.end();
-		}
-
-		/** test Lchild of this node */
-		bool hasLChild() const {
-			return neighbors.find(L) != neighbors.end();
-		}
-
-		/** test Rchild of this node */
-		bool hasRChild() const {
-			return neighbors.find(R) != neighbors.end();
-		}
-
-		/* Getters */
-		/** get parent of this node */
-		const PTNode& getParent() const {
-			return neighbors.find(P)->second;
-		}
-
-		/** get Lchild of this node */
-		const PTNode& getLChild() const {
-			return neighbors.find(L)->second;
-		}
-
-		/** get Rchild of this node */
-		const PTNode& getRChild() const {
-			return neighbors.find(R)->second;
-		}
-
-		/** test whether this node is a root node */
-		bool isRoot() const;
-
-		/** test whether this node is a leaf node */
-		bool isLeaf() const;
-
-		/** test whether this node is a tip node */
-		bool isTip() const;
-
-		DigitalSeq seq;
-		map<NodeType, PTNode> neighbors;
-		double length; /* branch length of this node (to its parent) */
-
-		Matrix4Xd cost; /* cost (negative log liklihood) of observing this sequence given the model and the tree */
-	};
-
+struct PhyloTree {
 	/* constructors */
-public:
-	/** Default constructor, only assign the alphabet for the entire tree */
-	PhyloTree() : abc(SeqCommons::nuclAbc) { }
+	/* Default constructor */
+	PhyloTree() : length(0), seq(NULL), parent(NULL) {
+		/* Assert IEE559 at construction time */
+		assert(std::numeric_limits<double>::is_iec559);
+	}
 
-	/* member methods */
-	/** test whether this tree is properly rooted */
-	bool isRooted() const;
+	/* Member methods */
+	/** test whether parent is initiated */
+	bool hasParent() const {
+		return parent != NULL;
+	}
 
-	/** test whether this tree is leaf rooted */
-	bool isLeafRooted() const;
+	/** test whether this SubTree is a leave root */
+	bool isLeafRoot() const {
+		return hasParent() && children.size() == 1;
+	}
 
-	/** test whether this tree is (arbitrarily) internally rooted */
-	bool isInternallyRooted() const;
+	/** test whether this SubTree is an internal root */
+	bool isInternalRoot() const {
+		return hasParent() && children.size() > 2;
+	}
+
+	/** test whether this SubTree is a root node */
+	bool isRoot() const {
+		return isLeafRoot() || isInternalRoot();
+	}
+
+	/** test whether this node is a leaf node */
+	bool isLeaf() const {
+		return children.empty();
+	}
+
+	/** test whether this node is a tip node */
+	bool isTip() const {
+		if(isLeaf())
+			return false;
+		/* test whether all children are leaves */
+		for(vector<PT>::const_iterator it = children.begin(); it != children.end(); ++it)
+			if(!it->isLeaf())
+				return false;
+		return true;
+	}
+
+	/** test whether the cost is evaluated */
+	bool isEvaluated() const {
+		return cost.cols() > 0 && (cost.array() != inf).any();
+	}
 
 	/** Get the number of aligned sites of this tree */
 	int alnSites() const;
 
 	/** Get the number of nodes of this tree */
 	int numNodes() const;
-
-	/** test whether two nodes are siblings */
-	bool isSibling(const PhyloTreeNode& node1, const PhyloTreeNode& node2) const;
-
-	/** Get the DFS set of nodes of a sub tree */
-	set<const PhyloTree::PhyloTreeNode*> dfsNodes(const PhyloTree::PhyloTreeNode* node) const;
-
-	/** Get the DFS set of this tree */
-	set<const PhyloTree::PhyloTreeNode*> dfsNodes() const;
-
-	/** Get the DFS set of leaf nodes of a sub tree */
-	set<const PhyloTree::PhyloTreeNode*> dfsLeaves(const PhyloTree::PhyloTreeNode* node) const;
-
-	/** Get the DFS set of leaf nodes of this tree */
-	set<const PhyloTree::PhyloTreeNode*> dfsLeaves() const;
-
-	/** Get the DFS set of tip nodes of a sub tree */
-	set<const PhyloTree::PhyloTreeNode*> dfsTips(const PhyloTree::PhyloTreeNode* node) const;
-
-	/** Get the DFS set of tip nodes of this tree */
-	set<const PhyloTree::PhyloTreeNode*> dfsTips() const;
-
-	/** Get the parent of a given node */
-	set<const PhyloTree::PhyloTreeNode*> parent(const PhyloTree::PhyloTreeNode* node) const {
-		return node->parent;
-	}
-
-	/** Get the children set of a given node */
-	set<const PhyloTree::PhyloTreeNode*> children(const PhyloTree::PhyloTreeNode* node) const;
-
-	/** Get the ancestor set of a given node */
-	set<const PhyloTree::PhyloTreeNode*> ancestors(const PhyloTree::PhyloTreeNode* node) const;
-
-	/** Get the offspring set of a given node */
-	set<const PhyloTree::PhyloTreeNode*> offsprings(const PhyloTree::PhyloTreeNode* node) const;
-
-	/** Get the sibling of given node of this tree */
-	const PhyloTreeNode* getSibling(const PhyloTreeNode*) const;
-	PhyloTreeNode* getSibling(const PhyloTreeNode*);
 
 	/**
 	 * Read a tree file and MSA into this object
@@ -174,92 +112,19 @@ public:
 
 	int readTreeNewick(const string& treefn, const MSA* msa);
 
-private:
-	void clear(); /* clear all nodes associated with this PhyloTree */
+	/* friend operators */
+	friend ostream& operator<<(ostream& out, const PT& tree);
 
-	PhyloTreeNode root;
-	const DegenAlphabet* abc;
-	//const DNASubModel* dnaModel;
-	//vector<PhyloTreeNode*> nodes;
+	/* member fields */
+	string name;
+	double length; /* branch length of this node */
+	vector<PT> children;
+	PT* parent;
+
+	const DigitalSeq* seq;
+	Matrix4Xd cost; /* cost (negative log liklihood) of observing this sequence given the model and the tree */
+
 };
-
-inline bool PhyloTree::PhyloTreeNode::isRoot() const {
-	return neighbors.size() == 3 /* an internally rooted tree */
-			|| neighbors.size() == 1 /* a leaf root */;
-}
-
-inline bool PhyloTree::PhyloTreeNode::isLeaf() const {
-	return hasLChild() && hasRChild();
-}
-
-inline bool PhyloTree::PhyloTreeNode::isTip() const {
-	return !isLeaf() && getLChild().isLeaf() && getRChild().isLeaf();
-}
-
-inline bool PhyloTree::isRooted() const {
-	return root.isRoot();
-}
-
-inline bool PhyloTree::isLeafRooted() const {
-	return root.neighbors.size() == 1;
-}
-
-inline bool PhyloTree::isInternallyRooted() const {
-	return root.neighbors.size() == 3;
-}
-
-inline int PhyloTree::alnSites() const {
-	return root.seq.length();
-}
-
-inline int PhyloTree::numNodes() const {
-	return dfsNodes().size();
-}
-
-inline PhyloTree::~PhyloTree() {
-	clear();
-}
-
-inline bool PhyloTree::isSibling(const PhyloTreeNode& node1, const PhyloTreeNode& node2) const {
-	assert(isRooted()); // only rooted tree can test siblings
-	return !node1.isRoot() && !node2.isRoot() && &node1.getParent() == &node2.getParent();
-}
-
-inline set<const PhyloTree::PhyloTreeNode*> PhyloTree::children(const PhyloTree::PhyloTreeNode* node) const {
-	set<const PhyloTree::PhyloTreeNode*> children;
-	children.insert(node->childL);
-	children.insert(node->childR);
-	return children;
-}
-
-inline set<const PhyloTree::PhyloTreeNode*> PhyloTree::ancestors(const PhyloTree::PhyloTreeNode* node) const {
-	set<const PhyloTree::PhyloTreeNode*> ancestors;
-	while(!node->isRoot()) {
-		ancestors.insert(node->parent);
-		node = node->parent;
-	}
-	return ancestors;
-}
-
-inline set<const PhyloTree::PhyloTreeNode*> PhyloTree::offsprings(const PhyloTree::PhyloTreeNode* node) const {
-	set<const PhyloTree::PhyloTreeNode*>& offsprings = dfsNodes(node);
-	offsprings.erase(node); /* remove itself */
-	return offsprings;
-}
-
-inline const PhyloTree::PhyloTreeNode* PhyloTree::getSibling(const PhyloTreeNode* node) const {
-	assert(isRooted()); /* only rooted tree can find siblings */
-	if(node->isRoot())
-		return NULL;
-	return node->parent->childL == node ? node->parent->childR : node->parent->childL;
-}
-
-inline PhyloTree::PhyloTreeNode* PhyloTree::getSibling(const PhyloTreeNode* node) {
-	assert(isRooted()); /* only rooted tree can find siblings */
-	if(node->isRoot())
-		return NULL;
-	return node->parent->childL == node ? node->parent->childR : node->parent->childL;
-}
 
 inline int PhyloTree::readTree(const string& treefn, const string& format, const MSA* msa) {
 	if(StringUtils::toLower(format) == "newick")
@@ -271,3 +136,76 @@ inline int PhyloTree::readTree(const string& treefn, const string& format, const
 } /* namespace EGriceLab */
 
 #endif /* SRC_PHYLOTREE_H_ */
+
+// adapt the structure to fusion phoenix
+BOOST_FUSION_ADAPT_STRUCT(
+	EGriceLab::PhyloTree,
+	(std::string, name)
+	(double, length)
+	(std::vector<EGriceLab::PT>, children)
+	(EGriceLab::PT*, parent)
+	(const EGriceLab::DigitalSeq*, seq)
+	(Eigen::Matrix4Xd, cost)
+)
+
+namespace EGriceLab {
+/* namespace aliasing */
+namespace qi = boost::spirit::qi;
+namespace phoenix = boost::phoenix;
+namespace fusion = boost::fusion;
+namespace ascii = boost::spirit::ascii;
+
+/* This generic grammar parse a generic iterator to a PhyloTree */
+template <typename Iterator>
+struct newick_grammar :
+		qi::grammar<Iterator, PT()> {
+	/* grammar constructor */
+	newick_grammar() : newick_grammar::base_type(tree) {
+		using phoenix::at_c;
+		using phoenix::push_back;
+
+		/* label grammar */
+		label %= qi::lexeme[+(qi::char_ - ':' - ')' - ',')]; /* strings without :, _ and , */
+
+		/* branch length grammar */
+		branch_length %= ':' >> qi::double_;
+
+		/* subtree grammar */
+		subtree =
+			// assign vector of children to the third element of PT, optional
+			-descendant_list  [at_c<2>(qi::_val) = qi::_1]
+			// assign the label to the first element, optional
+			>> -label  [at_c<0>(qi::_val) = qi::_1]
+			// assign the branch length to the second element, optional
+			>> -branch_length  [at_c<1>(qi::_val) = qi::_1];
+
+		/* descentdant_list is a vector of PT, which will be auto pushed to the compatible PT vector */
+		descendant_list %=
+			'(' >> subtree % ',' >> ')'; /* this grammar has the attribute of vector<PT> */
+
+		// The tree receive the whole subtree using %=
+		tree %= subtree >> ';';
+	}
+
+	private:
+	qi::rule<Iterator, PT()> tree;
+	qi::rule<Iterator, PT()> subtree;
+	qi::rule<Iterator, std::vector<PT>()> descendant_list;
+	qi::rule<Iterator, double()> branch_length;
+	qi::rule<Iterator, std::string()> label;
+
+};
+
+inline ostream& operator<<(ostream& out, const PT& tree) {
+	bool first = true;
+	out << "(" << tree.name << ": " << tree.length << " { ";
+	for(std::vector<PT>::const_iterator it = tree.children.begin(); it != tree.children.end(); ++it) {
+		out << (first ? "" : ",") << *it;
+		first = false;
+	}
+
+	out << " }";
+	return out;
+}
+
+} /* namespace EGriceLab */
