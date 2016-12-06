3d9c230ececdb07700580d09e3b5f1e5a7d209ea
zhengqi <zhengqi@gricelabstation1.MED.UPENN.EDU>
2016-12-06 10:38:14 -0500

    PhyloTreeUnrooted introduced as the base of tree evaluation

--- src/PhyloTreeUnrooted.cpp
+++ src/PhyloTreeUnrooted.cpp
@@ -5,7 +5,8 @@
  *      Author: zhengqi
  */
 
-
+#include <stack>
+#include <boost/unordered_set.hpp>
 #include "PhyloTreeUnrooted.h"
 
 namespace EGriceLab {
@@ -14,38 +15,110 @@ using namespace EGriceLab;
 
 const double PhyloTreeUnrooted::MIN_EXPONENT = std::numeric_limits<float>::min_exponent;
 
-int PhyloTreeUnrooted::numNodes() const {
-	return 0;
+PhyloTreeUnrooted::PhyloTreeUnrooted(const NewickTree& ntree) : L(0) {
+	/* DFS of the NewickTree */
+	boost::unordered_set<const NT*> visited;
+	stack<const NT*> S;
+	long id = 0; /* id start from 0 */
+
+	S.push(&ntree);
+	while(!S.empty()) {
+		const NT* v = S.top();
+		S.pop();
+		if(visited.find(v) == visited.end()) { /* not visited before */
+			visited.insert(v);
+			/* construct this PTUNode */
+			PTUNodePtr u(new PTUNode(id++, v->name));
+			id2node.push_back(u);
+
+			/* set root, if this is the first encounted node */
+			if(root == NULL)
+				root = u;
+
+			/* check each child of u(v) */
+			for(vector<NT>::const_iterator childIt = v->children.begin(); childIt != v->children.end(); ++childIt) {
+				/* construct a PTUNode based on this child */
+				PTUNodePtr child = PTUNodePtr(new PTUNode(id++, childIt->name));
+				/* update the child */
+				child->parent = u;
+				child->neighbors.push_back(u);
+				/* update the parent */
+				u->neighbors.push_back(child);
+
+				const NT* p = &*childIt;
+				S.push(p);
+			}
+		}
+	}
 }
 
-int PhyloTreeUnrooted::readNiwickTree(const string& treefn) {
-	namespace qi = boost::spirit::qi;
-
-	ifstream in(treefn.c_str());
-	if(!in.is_open()) {
-		cerr << "Error: Could not open input file: " << treefn << endl;
+long PhyloTreeUnrooted::loadMSA(const MSA& msa) {
+	const DegenAlphabet* abc = msa.getAbc();
+	if(abc != SeqCommons::nuclAbc) {
+		cerr << "PhyloTreeUnrooted can only read in MSA in dna alphabet" << endl;
 		return -1;
 	}
+	const unsigned numSeq = msa.getNumSeq();
+	const unsigned csLen = msa.getCSLen();
+	assert(numNodes() >= numSeq);
 
-	EGriceLab::newick_grammar<std::string::const_iterator> grammar;
-
-	string content;
-
-	/* copy the whole content in input */
-	in.unsetf(std::ios::skipws);
-	std::copy(std::istream_iterator<char>(in), std::istream_iterator<char>(), std::back_inserter(content));
-
-	string::const_iterator iter = content.begin();
-	string::const_iterator end = content.end();
-	// parse
-	bool result = qi::phrase_parse(iter, end, grammar, qi::space, *this);
-
-	if(result && iter == end)
-		return numNodes();
-	else {
-		cerr << "Parsing Newick tree failed" << endl;
-		return -1;
+	/* check uniqueness of seq names in msa */
+	map<string, unsigned> nameIdx;
+	for(unsigned i = 0; i != numSeq; ++i) {
+		string name = msa.seqNameAt(i);
+		if(nameIdx.find(name) != nameIdx.end()) {
+			cerr << "Non-unique seq name " << name << " found in your MSA data " << msa.getName() << endl;
+			return -1;
+		}
+		else {
+			nameIdx[name] = i;
+		}
 	}
+	/* assign seq to each nodes of the tree, exit early if cannot find */
+	for(vector<PTUNodePtr>::size_type id = 0; id != id2node.size(); ++id) {
+		const PTUNodePtr& node = id2node[id];
+		assert(id == node->id);
+
+		map<string, unsigned>::const_iterator result = nameIdx.find(node->name);
+		if(result == nameIdx.end()) { /* this name cannot be found in the msa */
+			cerr << "Tree node " << node->name << " does not exist in the MSA data" << endl;
+			return id; /* id can be 0 */
+		}
+		node->seq = msa.dsAt(nameIdx[node->name]);
+	}
+	return id2node.size();
+}
+
+PhyloTreeUnrooted::PTUNodePtr PhyloTreeUnrooted::setRoot(PTUNodePtr newRoot) {
+	if(newRoot == NULL || newRoot == root) /* no need to set */
+		return root;
+
+	newRoot->parent = NULL; // root has no parent
+	/* DFS of this tree starting from newRoot */
+	boost::unordered_set<PTUNodePtr> visited;
+	stack<PTUNodePtr> S;
+
+	S.push(newRoot);
+	while(!S.empty()) {
+		PTUNodePtr v = S.top();
+		S.pop();
+
+		if(visited.find(v) == visited.end()) { /* not visited before */
+			visited.insert(v);
+
+			/* check each child of v */
+			for(vector<PTUNodePtr>::iterator neighborIt = v->neighbors.begin(); neighborIt != v->neighbors.end(); ++neighborIt) {
+				if(*neighborIt == v) // this is neighbor is its parent, ignore
+					continue;
+				/* update the child's parent */
+				(*neighborIt)->parent = v;
+				S.push(*neighborIt);
+			}
+		}
+	}
+	PTUNodePtr oldRoot = root;
+	root = newRoot;
+	return oldRoot;
 }
 
 } /* namespace EGriceLab */
--- src/PhyloTreeUnrooted.h
+++ src/PhyloTreeUnrooted.h
@@ -1,10 +1,9 @@
 /*
  * PhyloTreeUnrooted.h
  *  An Unrooted Phylogenic Tree (PTUnrooted)
- *  A PTUnrooted can be rooted at any node
- *  A strict PTUnrooted node has exactly three (3) neighbors of its internal nodes
- *  When rooted, at most 1 neighbor is designated as its parent
- *  Tree nodes are number indexed internally
+ *  A PTUnrooted can be evaluated from any node as its root and yields same cost
+ *  as long as using a time-reversible DNA substitution model
+ *  Internal Tree nodes are number indexed from 0 to N-1
  *  Created on: Dec 1, 2016
  *      Author: zhengqi
  */
@@ -14,38 +13,33 @@
 
 #include <string>
 #include <vector>
-#include <set>
 #include <limits>
 #include <iostream>
 #include <stdexcept>
 #include <cstddef>
 #include <cassert>
 #include <eigen3/Eigen/Dense>
-#include <boost/spirit/include/qi.hpp>
-#include <boost/fusion/include/adapt_struct.hpp>
-#include <boost/variant/recursive_variant.hpp>
-#include <boost/spirit/include/phoenix_core.hpp>
-#include <boost/spirit/include/phoenix_operator.hpp>
-#include <boost/spirit/include/phoenix_fusion.hpp>
-#include <boost/spirit/include/phoenix_stl.hpp>
-#include <boost/spirit/include/phoenix_object.hpp>
 #include <boost/shared_ptr.hpp>
+#include <boost/unordered_map.hpp>
 
 #include "HmmUFOtuConst.h"
 #include "StringUtils.h"
 #include "SeqCommons.h"
 #include "DigitalSeq.h"
+#include "NewickTree.h"
 #include "MSA.h"
+#include "DNASubModel.h"
 
 namespace EGriceLab {
 using std::string;
 using std::vector;
-using std::set;
+using std::map;
 using std::istream;
 using std::ostream;
 using Eigen::Matrix4Xd;
 using Eigen::RowVectorXd;
 using boost::shared_ptr;
+using boost::unordered_map;
 
 class PhyloTreeUnrooted; /* forward declaration */
 
@@ -59,31 +53,32 @@ public:
 	typedef PTUnrooted::PhyloTreeUnrootedNode PTUNode;
 
 	typedef shared_ptr<PTUNode> PTUNodePtr; /* use boost shared_ptr to hold node pointers */
-	typedef shared_ptr<const PTUNode> PTUNodeConstPtr; /* a const version of boost shared_ptr */
+
 
 	/**
-	 * A PTUnrooed node that stores its neighbor pointers
-	 * if the tree is rooted, it always position 0 to store the parent
-	 * and position 1..N to store the children
+	 * A PTUnrooed node that stores its basic information and neighbors
 	 */
 	struct PhyloTreeUnrootedNode {
 		/* constructors */
 		/**
-		 * Default constructor, constructing a node with parent set to NULL
-		 * and parent length set to 0
-		 * and parent cost not initiated
+		 * Default constructor, do nothing
 		 */
-		PhyloTreeUnrootedNode() : hasParent(false) {
-			neighbors.push_back(PTUNodePtr());
-			length.push_back(0.0);
-			cost.push_back(Matrix4Xd());
-		}
+		PhyloTreeUnrootedNode() : id(0), annoDist(0) {	}
+
+		/**
+		 * Construct a PTUNode with a given name and id
+		 */
+		explicit PhyloTreeUnrootedNode(long id, const string& name = "")
+		: id(id), name(name), annoDist(0) {  }
+
+		/**
+		 * Construct a PTUNode with a given id, name and sequence
+		 */
+		PhyloTreeUnrootedNode(long id, const string& name, const DigitalSeq& seq)
+		: id(id), name(name), seq(seq), annoDist(0)
+		{ }
 
 		/* Member methods */
-		/** test whether this node is initiated */
-		bool isInit() const {
-			return !neighbors.empty();
-		}
 
 		/** test whether this node is named */
 		bool isNamed() const {
@@ -100,204 +95,106 @@ public:
 			return neighbors.size() > 1;
 		}
 
-		/** test whether this is a leaf root */
-		bool isLeafRoot() const {
-			return isLeaf() && !hasParent;
+		/** test whether this subtree is rooted */
+		bool isRooted() const {
+			return parent != NULL;
 		}
 
-		/** test whether this is an internal root */
-		bool isInternalRoot() const {
-			return isInternal() && !hasParent;
-		}
-
-		/** test whether this is a root */
-		bool isRoot() const {
-			return !hasParent;
-		}
-
-		/** test whether this node is a tip node */
-		bool isTip() const {
-			if(neighbors.empty())
-				return false;
-			/* test whether all children are leaves, ignore the parent */
-			for(vector<PTUNodePtr>::const_iterator child = neighbors.begin() + 1; child != neighbors.end(); ++child)
-				if(!(*child)->isLeaf())
-					return false;
-			return true;
-		}
-
-		/** reset the incoming cost of this node */
-		void resetCost();
-
-		/** reset the incoming cost of a neighbor */
-		void resetCost(int i) {
-			if(cost[i].cols() > 0)
-				cost[i].setConstant(inf);
-		}
-
-		/**
-		 * Get the cost of this node be merging incoming costs
-		 */
-		Matrix4Xd getCost() const;
-
+		/* member fields */
+		long id; /* a unique id for each node */
 		string name; /* node name, need to be unique for database loading */
 		DigitalSeq seq; /* sequence of this node */
 		vector<PTUNodePtr> neighbors; /* pointers to neighbors */
-		vector<double> length; /* branch lengths to neighbors */
-		bool hasParent; /* flag used when this node has a parent, which is always the first neighbor */
-		vector<Matrix4Xd> cost; /* incoming cost (message) from its children roots, when the tree is rooted */
+		PTUNodePtr parent; /* pointer to parent node, set to null on default */
+
+		string anno;
+		double annoDist;
 	};
 
-	PhyloTreeUnrooted() {
-		// TODO Auto-generated constructor stub
+	typedef unordered_map<PTUNodePtr, unordered_map<PTUNodePtr, Matrix4Xd> > CostMap;
+	typedef unordered_map<PTUNodePtr, unordered_map<PTUNodePtr, double> > BranchLenMap;
 
-	}
+	/* constructors */
+	/** Default constructor, do nothing */
+	PhyloTreeUnrooted() : L(0) {  }
+
+	/** Construct a PTUnrooted from a Newick Tree */
+	PhyloTreeUnrooted(const NewickTree& ntree);
+
+	/* member methods */
 
 	/** Get the number of nodes of this tree using Dfs search */
-	int numNodes() const;
+	long numNodes() const {
+		return id2node.size();
+	}
+
+	/** get number of aligned sites */
+	int getNumAlignSites() const {
+		return L;
+	}
+
+	/** Get root node */
+	const PTUNodePtr& getRoot() const {
+		return root;
+	}
+
+	/** Load sequences from MSA into this this */
+	long loadMSA(const MSA& msa);
 
 	/**
-	 * Read the tree structure (but not the sequence) from an input file of given format
-	 * @param treefn  tree filename
-	 * @param format  tree file format
-	 * @param msa  Multiple Sequence Alignment of this tree
-	 * @return  number of tree nodes read, or -1 if anything failed
-	 * @throw illegal_argument exception if is not a supported file format
+	 * save this object to an output in binary format
 	 */
-	int readTree(const string& treefn, const string& format);
+	ostream& save(ostream& out) const;
+
+	/** load a binary file into this object */
+	istream& load(string& in);
 
 	/**
-	 * Read the tree structure (but not the sequence) from a newick file
-	 * @param treefn  tree filename
-	 * @return
+	 * set tree root at given node, return the old node
 	 */
-	int readNiwickTree(const string& treefn);
+	PTUNodePtr setRoot(PTUNodePtr newRoot);
+
+	/**
+	 * test whether the cost (message) of node u -> v has been evaluated
+	 */
+	bool isEvaluated(const PTUNodePtr u, const PTUNodePtr v) const {
+		CostMap::const_iterator result = node2cost.find(u);
+		return result != node2cost.end() &&
+				result->second.find(v) != result->second.end();
+	}
+
+	/**
+	 * reset the evaluated cost
+	 * @return  actual number of cost removed
+	 */
+	CostMap::size_type resetCost(const PTUNodePtr u, const PTUNodePtr v) {
+		CostMap::iterator result = node2cost.find(u);
+		if(result == node2cost.end()) /* u not exists */
+			return 0;
+		return result->second.erase(v);
+	}
+
+	/**
+	 * evaluate the likelihood at node node by treating its the root of this PTUnrooted
+	 * @param node  new root of this tree
+	 * @param model  DNA substitution model, must be a time-reversible model
+	 * @return  overall cost of observing this tree at this root
+	 */
+	double evaluate(const PTUNodePtr node, const DNASubModel& model) const;
 
 private:
 	int L; /* number of aligned sites */
 
 	PTUNodePtr root; /* root node of this tree */
-	vector<PTUNodePtr> nodes; /* indexed tree nodes */
-	vector<string> anno; /* index node phylogenetic annotations */
-	vector<double> annoDist; /* index node annotation distance */
+	vector<PTUNodePtr> id2node; /* indexed tree nodes */
+
+	BranchLenMap id2length; /* branch length index storing all L*L pairs */
+	CostMap node2cost; /* cost index between two neighboring nodes */
+
 
 public:
 	/* static fields */
-	static const int INTERNAL_NEIGHBORS = 3; /* a PTUnrooted internal node always has 3 neighbors */
 	static const double MIN_EXPONENT;
-};
-
-inline void PhyloTreeUnrooted::PhyloTreeUnrootedNode::resetCost() {
-	assert(neighbors.size() == cost.size());
-	for(vector<PTUNodePtr>::size_type i = 0; i != neighbors.size(); ++i)
-		resetCost(i);
-}
-
-inline int PhyloTreeUnrooted::readTree(const string& treefn, const string& format) {
-	if(StringUtils::toLower(format) == "newick")
-		return readNiwickTree(treefn);
-	else {
-		std::cerr << "Unsupported tree file format '" << format << "'" << std::endl;
-		return -1;
-	}
-}
-
-
-} /* namespace EGriceLab */
-
-//// adapt the PTUNode to fusion phoenix
-//BOOST_FUSION_ADAPT_STRUCT(
-//	EGriceLab::PhyloTreeUnrooted::PhyloTreeUnrootedNode,
-//	(std::string, name)
-//	(EGriceLab::DigitalSeq, seq)
-//	(std::vector<EGriceLab::PhyloTreeUnrooted::PTUNodePtr>, neighbors)
-//	(std::vector<double>, length)
-//	(bool, hasParent)
-//	(std::vector<Eigen::Matrix4Xd>, cost)
-//)
-
-// contruct a PTUNode parser
-namespace EGriceLab {
-/* namespace aliasing */
-namespace qi = boost::spirit::qi;
-namespace phoenix = boost::phoenix;
-namespace fusion = boost::fusion;
-namespace ascii = boost::spirit::ascii;
-
-/* This generic grammar parse a generic iterator to a PTUNodePtr */
-template <typename Iterator>
-struct newick_grammar :
-		qi::grammar<Iterator, PTUnrooted::PTUNodePtr()> {
-	/* grammar constructor */
-	newick_grammar() : newick_grammar::base_type(tree, "Newick tree") {
-		using ascii::char_;
-		using ascii::string;
-		using phoenix::at_c;
-		using phoenix::push_back;
-		using qi::eps;
-		using qi::_val;
-		using qi::on_error;
-		using qi::fail;
-
-		using phoenix::construct;
-		using phoenix::ref;
-		using phoenix::val;
-
-		/* label grammars */
-		/* unquoted label is printable characters
-		 * without blanks, parentheses, brackets, single_quotes, colons, semicolons or commas
-		 */
-		unquoted_label %= qi::lexeme[+(ascii::print - ascii::space - '(' - ')' - '[' - ']' - '\'' - ':' - ';' - ',') ];
-		/* quoted label is ' printable characters ' */
-		quoted_label %= '\'' > qi::lexeme[+(ascii::print - '\'')] > '\'';
-
-		label = unquoted_label | quoted_label;
-
-		/* branch length grammar */
-		branch_length %= ':' > qi::double_;
-
-		/* subtree grammar */
-		subtree = eps [qi::_val = phoenix::new_<PTUnrooted::PTUNode> ] >> /* allocate the node */
-			// assign vector of children to the third element of PT, optional
-			-(descendant_list  ) /* get the neighbors, optional */
-			// assign the label, optional
-			>> -(label  )
-			// assign the branch length, optional
-			>> -(branch_length  );
-
-		/* descentdant_list is a vector of PTUNodePtr, which will be auto pushed to the compatible PTUNodePtr vector */
-		descendant_list %=
-			'(' >> subtree % ',' >> ')'; /* this grammar has the attribute of vector<PTUNodePtr> */
-
-		// The tree receive the whole subtree using %=
-		tree %= subtree >> ';';
-
-		unquoted_label.name("unquoted_label");
-		quoted_label.name("quoted_label");
-		label.name("label");
-		branch_length.name("branch_length");
-		subtree.name("subtree");
-		tree.name("tree");
-
-		on_error<fail>(
-				tree,
-				std::cout
-					<< val("Error! Expecting ")
-					<< qi::_4                             // what failed?
-					<< val(" here:\"")
-					<< construct<std::string>(qi::_3, qi::_2) // iterators to error-pos, end
-					<< val("\"")
-					<< std::endl
-				);
-	}
-
-	private:
-	qi::rule<Iterator, PTUnrooted::PTUNodePtr()> tree;
-	qi::rule<Iterator, PTUnrooted::PTUNodePtr()> subtree;
-	qi::rule<Iterator, std::vector<PTUnrooted::PTUNodePtr>()> descendant_list;
-	qi::rule<Iterator, double()> branch_length;
-	qi::rule<Iterator, std::string()> unquoted_label, quoted_label, label;
-
 };
 
 } /* namespace EGriceLab */
