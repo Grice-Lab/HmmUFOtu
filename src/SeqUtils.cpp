8e8442ae58e49a43649a5965dfa024821e5f8a16
zhengqi <zhengqi@gricelabstation1.MED.UPENN.EDU>
2017-05-11 10:16:32 -0400

    SeqUtils class introduced

--- src/SeqUtils.cpp
+++ src/SeqUtils.cpp
@@ -12,6 +12,7 @@ namespace EGriceLab {
 double SeqUtils::pDist(const DigitalSeq& seq1, const DigitalSeq& seq2,
 		DigitalSeq::size_type start, DigitalSeq::size_type end) {
 	assert(seq1.getAbc() == seq2.getAbc());
+	assert(seq1.length() == seq2.length());
 
 	DigitalSeq::size_type d = 0;
 	DigitalSeq::size_type N = 0;
@@ -25,6 +26,38 @@ double SeqUtils::pDist(const DigitalSeq& seq1, const DigitalSeq& seq2,
 		}
 	}
 	return static_cast<double>(d) / N;
+}
+
+double SeqUtils::pDist(const string& seq1, const string& seq2,
+		string::size_type start, string::size_type end) {
+	assert(seq1.length() == seq2.length());
+	string::size_type d = 0;
+	for(string::size_type i = start; i <= end; ++i)
+		if(seq1[i] != seq2[i])
+			d++;
+	return static_cast<double>(d) / (end - start + 1);
+}
+
+double SeqUtils::pDist(const string& seq1, const string& seq2,
+		const DegenAlphabet* abc, string::size_type start,
+		string::size_type end) {
+	assert(seq1.length() == seq2.length());
+	string::size_type d = 0;
+	for(string::size_type i = start; i <= end; ++i)
+		if(abc->encode(seq1[i]) == abc->encode(seq2[i]))
+			d++;
+	return static_cast<double>(d) / (end - start + 1);
+}
+
+double SeqUtils::pDist(const string& seq1, const DigitalSeq& seq2, size_t start,
+		size_t end) {
+	assert(seq1.length() == seq2.length());
+	const DegenAlphabet* abc = seq2.getAbc();
+	size_t d = 0;
+	for(size_t i = start; i <= end; ++i)
+		if(abc->encode(seq1[i]) == seq2[i])
+			d++;
+	return static_cast<double>(d) / (end - start + 1);
 }
 
 } /* namespace EGriceLab */
--- src/SeqUtils.h
+++ src/SeqUtils.h
@@ -9,7 +9,6 @@
 #define SRC_SEQUTILS_H_
 #include <string>
 #include "DigitalSeq.h"
-#include "PrimarySeq.h"
 
 namespace EGriceLab {
 
@@ -24,6 +23,33 @@ public:
 
 	/** calculate the p-distance between two aligned DigitalSeq */
 	static double pDist(const DigitalSeq& seq1, const DigitalSeq& seq2) {
+		return pDist(seq1, seq2, 0, seq1.length() - 1);
+	}
+
+	/** calculate the p-distance between two strings in a given region [start, end] */
+	static double pDist(const string& seq1, const string& seq2,
+			string::size_type start, string::size_type end);
+
+	/** calculate the p-distance between two strings */
+	static double pDist(const string& seq1, const string& seq2) {
+		return pDist(seq1, seq2, 0, seq1.length() - 1);
+	}
+
+	/** calculate the p-distance between two strings in a given region [start, end], allow Degenerated characters */
+	static double pDist(const string& seq1, const string& seq2, const DegenAlphabet* abc,
+			string::size_type start, string::size_type end);
+
+	/** calculate the p-distance between two strings */
+	static double pDist(const string& seq1, const string& seq2, const DegenAlphabet* abc) {
+		return pDist(seq1, seq2, abc, 0, seq1.length() - 1);
+	}
+
+	/** calculate the p-distance between two strings in a given region [start, end], allow Degenerated characters */
+	static double pDist(const string& seq1, const DigitalSeq& seq2,
+			size_t start, size_t end);
+
+	/** calculate the p-distance between two strings */
+	static double pDist(const string& seq1, const DigitalSeq& seq2) {
 		return pDist(seq1, seq2, 0, seq1.length() - 1);
 	}
 
