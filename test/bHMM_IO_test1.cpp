34e65ff0f20bd49c296bbd4b6551d775a599d679
zhengqi <zhengqi@gricelabstation1.MED.UPENN.EDU>
2017-05-31 11:17:37 -0400

    test message improved

--- test/bHMM_IO_test1.cpp
+++ test/bHMM_IO_test1.cpp
@@ -15,28 +15,37 @@ using namespace EGriceLab;
 
 int main(int argc, char *argv[]) {
 	if(argc != 3) {
-		cerr << "Usage:  " << argv[0] << " HMM-FILE OUTFILE" << endl;
+		cerr << "Usage:  " << argv[0] << " HMM-INFILE HMM-OUTFILE" << endl;
 		return 0;
 	}
 
 	ifstream in(argv[1]);
-	if(!in.is_open())
+	if(!in.is_open()) {
 		cerr << "Unable to open " << argv[1] << endl;
+		return EXIT_FAILURE;
+	}
 	ofstream out(argv[2]);
-	if(!out.is_open())
+	if(!out.is_open()) {
 		cerr << "Unable to write to " << argv[2] << endl;
+		return EXIT_FAILURE;
+	}
 
 	BandedHMMP7 hmm;
-	cerr << "hmm constructed" << endl;
 	in >> hmm;
 	if(in.good())
 		cerr << "Hmm read" << endl;
+	else {
+		cerr << "Unable to read Hmm file: " << argv[1] << endl;
+		return EXIT_FAILURE;
+	}
 
 	out << hmm;
 	if(out.good())
 		cerr << "Hmm writtten" << endl;
+	else {
+		cerr << "Unable to write to Hmm file: " << argv[2] << endl;
+		return EXIT_FAILURE;
+	}
 
-	in.close();
-	out.close();
 	return 0;
 }
