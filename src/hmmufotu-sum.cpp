c3b6e2bf699664725473c643e5b7f5b6505d3ada
Qi Zheng <zhengqi@upenn.edu>
2017-08-05 07:12:29 -0400

    normalizing and sutsetting function moved away from hmmufotu-sum, but -r option added

--- src/hmmufotu-sum.cpp
+++ src/hmmufotu-sum.cpp
@@ -19,7 +19,7 @@
  *******************************************************************************/
 /*
  * hmmufotu-sum.cpp
- *
+ *  summarize OTUTable from hmmufotu assignment files
  *  Created on: Apr 7, 2017
  *      Author: Qi Zheng
  *      Version: v1.1
@@ -55,10 +55,8 @@ static const int DEFAULT_MIN_NSAMPLE = 0;
 static const double DEFAULT_MIN_Q = 0;
 static const double DEFAULT_MIN_ALN_IDENTITY = 0;
 static const double DEFAULT_MIN_HMM_IDENTITY = 0;
-static const double DEFAULT_NORM = 0;
-static const unsigned long DEFAULT_SUBSET = 0;
-static const string DEFAULT_SUBSET_METHOD = "uniform";
 typedef boost::unordered_map<PTUnrooted::PTUNodePtr, OTUObserved> OTUMap;
+typedef boost::unordered_map<string, vector<string>> OTU2ReadMap;
 
 /**
  * Print introduction of this program
@@ -73,7 +71,8 @@ void printIntro(void) {
 void printUsage(const string& progName) {
 	cerr << "Usage:    " << progName << "  <HmmUFOtu-DB> <(INFILE [INFILE2 ...]> <-o OTU-OUT> [options]" << endl
 		 << "INFILE          FILE           : assignment file(s) from hmmufotu, by default the filenames will be used as sample names" << endl
-		 << "Options:    -o  FILE           : OTU summary output" << endl
+		 << "Options:    -o  FILE           : OTU summary output, required" << endl
+		 << "            -r  FILE           : output the read IDs for each OTU" << endl
 		 << "            -l  FILE           : an optional sample list, with 1st field sample-name and 2nd field input filename, only input files in list will be used" << endl
 		 << "            -c  FILE           : OTU Consensus Sequence (CS) alignment output" << endl
 		 << "            -t  FILE           : OTU tree output" << endl
@@ -83,11 +82,6 @@ void printUsage(const string& progName) {
 		 << "            -e|--effN  DBL     : effective number of sequences (pseudo-count) for inferencing CS of OTUs with Dirichelet Density models, set 0 to disable [" << DEFAULT_EFFN << "]" << endl
 		 << "            -n  INT            : minimum number of observed reads required to define an OTU across all samples, 0 for no filtering [" << DEFAULT_MIN_NREAD << "]" << endl
 		 << "            -s  INT            : minimum number of observed samples required to define an OTU, 0 for no filtering [" << DEFAULT_MIN_NSAMPLE << "]" << endl
-		 << "            -N|--norm  FLAG    : apply constant normalization after calculating the OTU Table" << endl
-		 << "            -Z  DBL            : normalization factor for -N, set to 0 to use the default value [" << DEFAULT_NORM << "]" << endl
-		 << "            --subset  LONG     : subsample each sample to reduce the read count to GIVEN VALUE, 0 for not subsampling [" << DEFAULT_SUBSET << "]" << endl
-		 << "            --sub-method  STR  : subsampling method if --subset requested, either 'uniform' (wo/ replacement) or 'multinomial' (w/ placement) [" << DEFAULT_SUBSET_METHOD << "]" << endl
-		 << "            -S|--seed  INT     : random seed used for --subset, for debug purpose" << endl
 		 << "            -v  FLAG           : enable verbose information, you may set multiple -v for more details" << endl
 		 << "            -h|--help          : print this message and exit" << endl;
 }
@@ -98,10 +92,11 @@ int main(int argc, char* argv[]) {
 	vector<string> inFiles;
 	map<string, string> sampleFn2Name;
 	string listFn;
-	string otuFn, csFn, treeFn;
+	string otuFn, readFn, csFn, treeFn;
 	ifstream msaIn, hmmIn, ptuIn;
-	ofstream otuOut, treeOut, csOut;
+	ofstream otuOut, readOut, treeOut, csOut;
 	SeqIO csO;
+	OTU2ReadMap otu2Read;
 
 	double effN = DEFAULT_EFFN;
 	double minQ = DEFAULT_MIN_Q;
@@ -110,10 +105,6 @@ int main(int argc, char* argv[]) {
 	int minRead = DEFAULT_MIN_NREAD;
 	int minSample = DEFAULT_MIN_NSAMPLE;
 	bool doNorm = false;
-	double normZ = DEFAULT_NORM;
-	unsigned long subN = DEFAULT_SUBSET;
-	string subMethod = DEFAULT_SUBSET_METHOD;
-	unsigned seed = time(NULL); // using time as default seed
 
 	/* parse options */
 	CommandOptions cmdOpts(argc, argv);
@@ -141,6 +132,8 @@ int main(int argc, char* argv[]) {
 		cerr << "-o must be specified" << endl;
 		return EXIT_FAILURE;
 	}
+	if(cmdOpts.hasOpt("-r"))
+		readFn = cmdOpts.getOpt("-r");
 	if(cmdOpts.hasOpt("-c"))
 		csFn = cmdOpts.getOpt("-c");
 	if(cmdOpts.hasOpt("-t"))
@@ -166,21 +159,6 @@ int main(int argc, char* argv[]) {
 	if(cmdOpts.hasOpt("-s"))
 		minSample = ::atoi(cmdOpts.getOptStr("-s"));
 
-	if(cmdOpts.hasOpt("-N") || cmdOpts.hasOpt("--norm"))
-		doNorm = true;
-	if(cmdOpts.hasOpt("-Z"))
-		normZ = ::atof(cmdOpts.getOptStr("-Z"));
-
-	if(cmdOpts.hasOpt("--subset"))
-		subN = ::atol(cmdOpts.getOptStr("--subset"));
-	if(cmdOpts.hasOpt("--sub-method"))
-		subMethod = cmdOpts.getOpt("--sub-method");
-
-	if(cmdOpts.hasOpt("-S"))
-		seed = ::atoi(cmdOpts.getOptStr("-S"));
-	if(cmdOpts.hasOpt("--seed"))
-		seed = ::atoi(cmdOpts.getOptStr("--seed"));
-
 	if(cmdOpts.hasOpt("-v"))
 		INCREASE_LEVEL(cmdOpts.getOpt("-v").length());
 
@@ -195,10 +173,6 @@ int main(int argc, char* argv[]) {
 	}
 	if(!(minSample >= 0)) {
 		cerr << "-s must be non-negative integer" << endl;
-		return EXIT_FAILURE;
-	}
-	if(normZ < 0) {
-		cerr << "-Z must be non-negative" << endl;
 		return EXIT_FAILURE;
 	}
 
@@ -257,6 +231,14 @@ int main(int argc, char* argv[]) {
 	if(!otuOut.is_open()) {
 		cerr << "Unable to write to '" << otuFn << "': " << ::strerror(errno) << endl;
 		return EXIT_FAILURE;
+	}
+
+	if(!readFn.empty()) {
+		readOut.open(readFn.c_str());
+		if(!readOut.is_open()) {
+			cerr << "Unable to write to '" << readFn << "': " << ::strerror(errno) << endl;
+			return EXIT_FAILURE;
+		}
 	}
 
 	if(!csFn.empty()) {
@@ -328,6 +310,7 @@ int main(int argc, char* argv[]) {
 		sampleNames.push_back(sample);
 		while(tsvIn.hasNext()) {
 			const TSVRecord& record = tsvIn.nextRecord();
+			string rid = record.getFieldByName("id");
 			int csStart = ::atoi(record.getFieldByName("CS_start").c_str());
 			int csEnd = ::atoi(record.getFieldByName("CS_end").c_str());
 			const string& aln = record.getFieldByName("alignment");
@@ -338,10 +321,13 @@ int main(int argc, char* argv[]) {
 					&& EGriceLab::alignIdentity(abc, aln, csStart - 1, csEnd -1)
 					&& EGriceLab::hmmIdentity(hmm, aln, csStart - 1, csEnd - 1)) { /* a valid assignment */
 				const PTUnrooted::PTUNodePtr& node = ptu.getNode(taxon_id);
+				string otuID = boost::lexical_cast<string>(node->getId());
 				if(otuData.count(node) == 0) /* not initiated */
-					otuData.insert(std::make_pair(node, OTUObserved(boost::lexical_cast<string>(node->getId()), node->getTaxon(), L, S)));
+					otuData.insert(std::make_pair(node, OTUObserved(otuID, node->getTaxon(), L, S)));
 				OTUObserved& otu = otuData.find(node)->second;
 				otu.count(s)++;
+				if(readOut.is_open())
+					otu2Read[otuID].push_back(rid);
 				for(int j = 0; j < L; ++j) {
 					int8_t b = abc->encode(::toupper(aln[j]));
 					if(b >= 0)
@@ -364,28 +350,24 @@ int main(int argc, char* argv[]) {
 		OTUObserved& otu = otuData.find(node)->second;
 		if(otu.numReads() >= minRead && otu.numSamples() >= minSample) /* filter OTUs */
 			otuTable.addOTU(otu);
+		else {/* remove unnessesary otu2Read elements */
+			if(readOut.is_open())
+				otu2Read.erase(otu.id);
+		}
 	}
-
-	/* OTUTable process */
-	if(doNorm) {
-		infoLog << "Normalizing OTU Table" << endl;
-		otuTable.normalize(normZ);
-	}
-
-	if(subN > 0) {
-		infoLog << "Subsampling OTU Table" << endl;
-		otuTable.seed(seed);
-		otuTable.subset(subN, subMethod);
-
-		/* prune OTUTable */
-		otuTable.pruneSamples(subN);
-		otuTable.pruneOTUs(minRead);
-	}
-
 
 	/* write the OTU table */
 	infoLog << "Writing OTU Table" << endl;
 	otuOut << otuTable;
+
+	/* write read list */
+	if(readOut.is_open()) {
+		infoLog <<"Wring read ID lists" << endl;
+		for(size_t i = 0; i <otuTable.numOTUs(); ++i) {
+			string otuID = otuTable.getOTU(i);
+			readOut << otuID <<"\t" << boost::join(otu2Read[otuID], " ") << endl;
+		}
+	}
 
 	/* write the CS tree */
 	if(csOut.is_open()) {
