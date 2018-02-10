4e09f22086d42f848ffa0259ea71b11241f04734
Qi Zheng <zhengqi@upenn.edu>
2018-02-10 13:25:18 -0500

    Revert "Introducing the new PTSegPlacement class"
    This reverts commit 15dbeedd31e62c36702b9fe968916b162dd9e8dc.

--- src/HmmUFOtu_main.h
+++ src/HmmUFOtu_main.h
@@ -90,14 +90,6 @@ vector<PTUnrooted::PTLoc> getSeed(const PTUnrooted& ptu, const DigitalSeq& seq,
 vector<PTUnrooted::PTPlacement> estimateSeq(const PTUnrooted& ptu, const DigitalSeq& seq,
 		const vector<PTUnrooted::PTLoc>& locs, const string& method);
 
-/** Get estimated placement for a seq segment at one PTLoc */
-PTSegPlacement estimateSeg(const PTUnrooted& ptu, const DigitalSeq& seq, int start, int end,
-		const PTLoc& segLoc, const string& method);
-
-/** Get estimated placement for a seq at given locations */
-vector<PTSegPlacement> estimateSeq(const PTUnrooted& ptu, const DigitalSeq& seq, int start, int end,
-		const vector<PTLoc>& segLocs, const string& method);
-
 /**
  * filter estimated placement by removing bad placement with estimated loglik lower than the best placement
  * @param places  a vector of placements
